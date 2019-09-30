defc4a2d46211ee5614095fb4cd980316b3759e2
Steve Meier <email@steve-meier.de>
2019-10-01 00:36:41 +0200

    Update spfcheck.pl
    Move from Mail::RBL::Query to Mail::RBL
    Mail::RBL is readily packaged and supports IPv6

--- spfcheck.pl
+++ spfcheck.pl
@@ -2,15 +2,7 @@
 
 use strict;
 use warnings;
-use Mail::SPF::Query;
-
-my $query = new Mail::SPF::Query (ip => $ENV{'TCPREMOTEIP'}, sender=>$ENV{'SMTPMAILFROM'}, helo=>$ENV{'SMTPHELOHOST'}, trusted=>1, guess=>1);
-
-my ($result,           # pass | fail | softfail | neutral | none | error | unknown [mechanism]
-    $smtp_comment,     # "please see http://spf.pobox.com/why.html?..."  when rejecting, return this string to the SMTP client
-    $header_comment,   # prepend_header("Received-SPF" => "$result ($header_comment)")
-    $spf_record,       # "v=spf1 ..." original SPF record for the domain
-   ) = $query->result2();
+use Mail::SPF;
 
 # Relay- and Trustclients pass right through
 if ( (defined($ENV{'RELAYCLIENT'})) ||
@@ -19,14 +11,24 @@ if ( (defined($ENV{'RELAYCLIENT'})) ||
   exit 0;
 }
 
+# Make sure that SMTPHELOHOST is set
+if (not(defined($ENV{'SMTPHELOHOST'}))) { print "\n"; exit 0; };
+
+my $spf_server = Mail::SPF::Server->new();
+my $request = Mail::SPF::Request->new(ip_address    => $ENV{'TCPREMOTEIP'},
+                                      identity      => $ENV{'SMTPMAILFROM'},
+                                      helo_identity => $ENV{'SMTPHELOHOST'},
+				      scope         => 'mfrom');
+
+my $result = $spf_server->process($request);
+
 # Reject the client if SPF check returns "fail"
-if ($result eq 'fail') {
-  print STDERR getppid()." SPF check failed for $ENV{'SMTPMAILFROM'} !\n";
+if ($result->code eq 'fail') {
+  print STDERR getppid()." SPF check failed for $ENV{'SMTPMAILFROM'}\n";
   print STDERR getppid()." Mail recipient would have been $ENV{'SMTPRCPTTO'}\n";
-  print "E451 SPF check failed: $smtp_comment\n";
+  print "E451 SPF check failed: ".$result->local_explanation."\n";
 } else {
   print "\n";
 }
 
 exit;
-
