9ce75ba71ea5d71e355356d7c2050453af8e55d3
stevemeier <email@steve-meier.de>
2019-10-17 18:16:01 +0200

    Commit current deliver.pl from prod

--- deliver.pl
+++ deliver.pl
@@ -6,18 +6,20 @@
 use strict;
 use warnings;
 
-use Digest::SHA1 qw(sha1_hex);
+use Carp;
+use Digest::SHA qw(sha1_hex);
 use File::Find;
 use Time::Local;
 use Time::gmtime;
 use Sys::Hostname;
 use DBI;
+use DBD::mysql;
 
 # Pathes and external binaries
 # Adjust as necessary
 # ****************************
 my $configdir = "/etc/qbox";
-my $spamcbin = "/usr/bin/spamc";
+my $spamcbin = "/bin/spamc";
 my $vscanbin = "/opt/f-prot/f-prot";
 my $tempdir = "/tmp";
 my $quarantine = "/var/qmail/quarantine";
@@ -30,7 +32,6 @@ my $sqlrows;
 my $statement;
 my $error = 111;
 my $isduplicate = 0;
-my @rcpts;
 my $homedir;
 my $quota;
 my $diskusage;
@@ -44,12 +45,9 @@ umask(077);
 
 # Check for prerequisites ($RECIPIENT)
 # ************************************
-#if ( (not($ENV{'RECIPIENT'})) || (not($ENV{'SENDER'})) ) {
 if (not($ENV{'RECIPIENT'})) {
-  #print "\$RECIPIENT or \$SENDER not set !\n";
   print "\$RECIPIENT not set !\n";
-  $error = 1;
-  &clean_exit();
+  exit(1);
 }
 
 # Connect to MySQL database
@@ -65,9 +63,9 @@ my $pid = $$;
 my $hostname = hostname();
 my $sender = $ENV{'SENDER'};
 my $email = $ENV{'RECIPIENT'};
-$email =~ s/qbox-//;					# ausgliedern !
+$email =~ s/qbox-//x;                   # ausgliedern !
 chomp($email);
-my ($user,$domain) = split(/\@/, $email);
+my ($user,$domain) = split(/\@/x, $email);
 chomp($user);
 chomp($domain);
 
@@ -96,7 +94,7 @@ if (not(defined($homedirs[0]))) {
 
 # Read the message from STDIN
 # ***************************
-while (<STDIN>) {
+while (<>) {
   $message .= $_;
 }
 $msgsize = length($message);
@@ -128,7 +126,7 @@ if ($sqlrows > 0) {
 # *******************
 &debug("homedirs: @homedirs\n");
 
-foreach $homedir (@homedirs) {
+foreach my $homedir (@homedirs) {
 
   if (defined($quotas{$homedir})) {
     $quota = $quotas{$homedir};
@@ -136,13 +134,13 @@ foreach $homedir (@homedirs) {
     $quota = 0;
   }
 
-  if ($homedir =~ /\@/) {
+  if ($homedir =~ /\@/x) {
     &forward($homedir);
   }
-  if ($homedir =~ /^\|/) {
-    &pipe($homedir);
+  if ($homedir =~ /^\|/x) {
+    &write_pipe($homedir);
   }
-  if ($homedir =~ /^\//) {
+  if ($homedir =~ /^\//x) {
     if ($quota > 0) {
       &check_quota($homedir);
         if ($error != 1) {
@@ -163,7 +161,7 @@ if ($error == 0) {
 
 # Murphys law: something went wrong...
 # ************************************
-sub clean_exit() {
+sub clean_exit {
   $sth->finish;
   $dbh->disconnect;
   if ($error != 0) { print "Exiting with code $error\n"; };
@@ -174,8 +172,8 @@ sub clean_exit() {
 # MODULES START HERE...
 # *********************
 
-sub maildir() {
-  $homedir = $_[0];
+sub maildir {
+  my ($lhomedir) = @_;
   my $subdir = "INBOX";
   my $timestamp = timelocal(localtime);
   my $filename = "$timestamp\.$pid\.$hostname\.$msghash";
@@ -187,7 +185,7 @@ sub maildir() {
   &make_query();
   if ($sqlrows > 0) {
     &debug("Searching for duplicates with hash $msghash\n");
-    find(\&duplicate, $homedir);
+    find(\&duplicate, $lhomedir);
     if ($isduplicate == 1) {
       # we have found a duplicate
       print "Message discarded due to duplicate filter\n";
@@ -201,31 +199,31 @@ sub maildir() {
   if ($sqlrows > 0) {
     ($spamdir, $spamlimit) = $sth->fetchrow_array();
   }
-  if ( ($message =~ /\nX-Spam-Level:\s+\+{$spamlimit,}/) && ($spamdir) ) {
+  if ( ($message =~ /\nX-Spam-Level:\s+\+{$spamlimit,}/x) && ($spamdir) ) {
     $subdir = $spamdir;
   }
 
-  $SIG{ALRM} = sub { die "timeout" };
+  local $SIG{ALRM} = sub { croak "timeout" };
 
   eval {
     alarm(86400);
-    if (not(-d $homedir)) {
+    if (not(-d $lhomedir)) {
       &create_homedir();
     }
-    chdir("$homedir") || &defer("ERROR: Unable to chdir to $homedir\n");
-    chdir("$subdir") || &defer("ERROR: Unable to chdir to $homedir\/$subdir !\n");
-    open(OUTPUT, "> tmp/$filename") || &defer("ERROR: Unable to create file in $homedir/tmp !\n");
+    chdir("$lhomedir") || &defer("ERROR: Unable to chdir to $lhomedir\n");
+    chdir("$subdir") || &defer("ERROR: Unable to chdir to $lhomedir/$subdir !\n");
+    open(my $OUTPUTFH, ">", "tmp/$filename") || &defer("ERROR: Unable to create file in $lhomedir/tmp !\n");
   
-    print OUTPUT $message;
+    print $OUTPUTFH $message;
 
-    close(OUTPUT) || &defer("ERROR: Unable to close tmp file !\n");
+    close($OUTPUTFH) || &defer("ERROR: Unable to close tmp file !\n");
     link("tmp/$filename","new/$filename") || &defer("ERROR: Unable to link() !\n");
     unlink("tmp/$filename") || &defer("ERROR: Unable to unlink() !\n");
-    print "Message delivered to $homedir\/$subdir for $email\n";
+    print "Message delivered to $lhomedir\/$subdir for $email\n";
     $error = 0;
     alarm(0);
     if ($quota > 0) {
-      &update_quota($homedir);
+      &update_quota($lhomedir);
     }
   };
 
@@ -238,48 +236,55 @@ sub maildir() {
 #     die;
     }
   }
+
+  return;
 }
 
-sub forward() {
-  $homedir = $_[0];
+sub forward {
+  my ($lhomedir) = @_;
 
-  open(INJECT, "|/var/qmail/bin/qmail-inject -f$sender $homedir");
-    print INJECT $message;
-  close(INJECT);
+  open(my $INJECTFH, "|-", "/var/qmail/bin/qmail-inject -f$sender $lhomedir") || croak "Could not invoke qmail-inject";
+    print $INJECTFH $message;
+  close($INJECTFH);
 
   if ($? == 0) {
-    print "Message forwarded to $homedir\n";
+    print "Message forwarded to $lhomedir\n";
     $error = 0;
   } else {
-    print "ERROR: Forwarding to $homedir failed !\n";
+    print "ERROR: Forwarding to $lhomedir failed !\n";
     $error = 111;
   }
+
+  return;
 }  
 
-sub pipe() {
-  $homedir = $_[0];
+sub write_pipe {
+  my ($lhomedir) = @_;
+  $lhomedir =~ s/\|//x;
 
   if (-x $homedir) {
-    my $pid = open(PIPE, "$homedir") || warn "ERROR: Could not fork $homedir => $!\n";
-    if ($pid) {
-      print PIPE $message;
-      close(PIPE);  
+    my $lpid = open(my $PIPEFH, "|-", "$lhomedir") || warn "ERROR: Could not fork $lhomedir => $!\n";
+    if ($lpid) {
+      print $PIPEFH $message;
+      close($PIPEFH);  
     }
   
     if ($? == 0) {
-      print "Message piped to $homedir\n";
+      print "Message piped to $lhomedir\n";
       $error = 0;
     } else {
-      print "ERROR: Piping to $homedir failed !\n";
+      print "ERROR: Piping to $lhomedir failed !\n";
       $error = 111;
     }
   } else {
-    print "ERROR: $homedir not found or not executable !\n";
+    print "ERROR: $lhomedir not found or not executable !\n";
     $error = 111;
   }
+
+  return;
 }
 
-sub get_homedir() {
+sub get_homedir {
   $statement = "select distinct passwd.homedir,passwd.quota from passwd inner join mapping on passwd.uid = mapping.uid where user = \"$user\" and domain = \"$domain\"";
   &make_query();
 
@@ -288,9 +293,11 @@ sub get_homedir() {
     push(@homedirs, $data[0]);
     %quotas = ($data[0] => $data[1]);
   }
+
+  return;
 }
 
-sub make_query() {
+sub make_query {
   $sqlrows = 0;
   $sth = $dbh->prepare($statement);
   $success = $sth->execute();
@@ -304,54 +311,66 @@ sub make_query() {
   return $success;
 }
 
-sub check_quota() {
-  if (not( -f "$_[0]\/.mailquota" )) {
-    open(MAILQUOTA, "> $_[0]\/.mailquota");
-      print MAILQUOTA "0";
-    close(MAILQUOTA);
+sub check_quota {
+  my ($lhomedir) = @_;
+
+  if (not( -f "$lhomedir/.mailquota" )) {
+    open(my $MAILQUOTAFH, ">", "$lhomedir/.mailquota") || croak "Could not write to $lhomedir/.mailquota";
+      print $MAILQUOTAFH "0";
+    close($MAILQUOTAFH);
   }
 
-  open(MAILQUOTA, "$_[0]\/.mailquota");
-    $diskusage = <MAILQUOTA>;
-  close(MAILQUOTA);
+  open(my $MAILQUOTAFH, ">", "$lhomedir/.mailquota") || croak "Could not write to $lhomedir/.mailquota";
+    $diskusage = <$MAILQUOTAFH>;
+  close($MAILQUOTAFH);
 
   if ($diskusage > $quota) {
     print "ERROR: Mailbox full ! Maximum Quota ($quota KB) exeeded.\n";
     $error = 1;
   }
+
+  return;
 }
 
-sub update_quota() {
+sub update_quota {
+  my ($lhomedir) = @_;
   my $homesize = 0;
-  find(sub { $homesize += -s if -f $_ }, " $_[0]");
-  open(MAILQUOTA, "> $_[0]\/.mailquota");
-    print MAILQUOTA $homesize + $msgsize;
-  close(MAILQUOTA);
+
+  find(sub { $homesize += -s if -f $_ }, " $lhomedir");
+
+  open(my $MAILQUOTAFH, ">", "$lhomedir/.mailquota") || croak "Could not write to $lhomedir/.mailquota";
+    print $MAILQUOTAFH $homesize + $msgsize;
+  close($MAILQUOTAFH);
+
+  return;
 }
 
-sub defer() {
-  print @_;
+sub defer {
+  my ($lmessage) = @_;
+  print $lmessage;
   exit(111);
 }
 
-sub create_homedir() {
+sub create_homedir {
   mkdir("$homedir");
   mkdir("$homedir/INBOX");
   mkdir("$homedir/INBOX/tmp");
   mkdir("$homedir/INBOX/new");
   mkdir("$homedir/INBOX/cur");
+
+  return;
 }
 
-sub autorespond() {
+sub autorespond {
   $statement = "select distinct passwd.artext,passwd.uid from passwd inner join mapping on passwd.uid = mapping.uid where user = \"$user\" and domain = \"$domain\" and arstart > 0 and arend >= UNIX_TIMESTAMP()";
   &make_query();
 
   # This handles RFC2919
-  if ($message =~ /\nList-ID:/i) {
+  if ($message =~ /\nList-ID:/ix) {
     $sqlrows = 0;
   }
 
-  if (not($message =~ /\nX-Mailer:/i)) {
+  if (not($message =~ /\nX-Mailer:/ix)) {
     $sqlrows = 0;
   }
 
@@ -362,7 +381,7 @@ sub autorespond() {
     &make_query();
 
     my $subject;
-    if ($message =~ /\nSubject: (.*)\n/) {
+    if ($message =~ /\nSubject: (.*)\n/x) {
       # RFC3834 recommends this
       $subject = "Subject: Auto: $1\n";
     } else {
@@ -370,59 +389,63 @@ sub autorespond() {
     }
 
     if ( ($email && $sender) && ($sqlrows == 0) ) {
-      open(INJECT, "|/var/qmail/bin/qmail-inject -f$email $sender");
-        print INJECT "From: \<$email\>\n";
-        print INJECT "To: \<$sender\>\n";
-        print INJECT $subject;
-        print INJECT "Message-ID: \<".timelocal(localtime)."\.$pid\@$hostname\>\n";
-        print INJECT "Date: ".&rfc2822_date()."\n\n";
-        print INJECT $artext;
-        #print INJECT "\n\nYour original message follows:\n\n";
-        #print INJECT $message; 
-      close(INJECT);
+      open(my $INJECTFH, "|-", "/var/qmail/bin/qmail-inject -f$email $sender") || croak "Could not call qmail-inject";
+        print $INJECTFH "From: \<$email\>\n";
+        print $INJECTFH "To: \<$sender\>\n";
+        print $INJECTFH $subject;
+        print $INJECTFH "Message-ID: \<".timelocal(localtime)."\.$pid\@$hostname\>\n";
+        print $INJECTFH "Date: ".&rfc2822_date()."\n\n";
+        print $INJECTFH $artext;
+      close($INJECTFH);
 
       $statement = "insert into responses values ('','$aruid','$sender',UNIX_TIMESTAMP())";
       &make_query();
     }
   }
+
+  return;
 }
 
-sub spam_assassin() {
+sub spam_assassin {
   if (-x $spamcbin) {
     my $timestamp = timelocal(localtime);
     my $tempfile = "$tempdir\/$timestamp\.$pid\.$hostname";
   
-    open(SPAMC, "| $spamcbin > $tempfile") || warn "ERROR forking $spamcbin !\n";
-      print SPAMC $message;
-    close(SPAMC);
+    open(my $SPAMCFH, "|-", "$spamcbin > $tempfile") || warn "ERROR forking $spamcbin !\n";
+      print $SPAMCFH $message;
+    close($SPAMCFH);
 
     if ((-s $tempfile) > $msgsize) {
       $message = '';
-      open(SARESULT, $tempfile);
-      while(<SARESULT>) {
+      open(my $SARESULTFH, "<", $tempfile) || carp "Could not read result file";
+      while(<$SARESULTFH>) {
         $message .= $_;
       }
-      close(SARESULT);
+      close($SARESULTFH);
       $msgsize = length($message);
       unlink($tempfile);
     }
   }
+
+  return;
 }
 
-sub virus_scan() {
+sub virus_scan {
   if (-x $vscanbin) {
     my $timestamp = timelocal(localtime);
     my $tempfile = "$tempdir\/$timestamp\.$pid\.$hostname";
 
-    open(TEMP, "> $tempfile") || warn "ERROR writing $tempfile !\n";
-      print TEMP $message;
-    close(TEMP);
+    open(my $TEMPFH, ">", $tempfile) || warn "ERROR writing $tempfile !\n";
+      print $TEMPFH $message;
+    close($TEMPFH);
 
     system("$vscanbin $tempfile 2>&1 >/dev/null"); # || warn "ERROR forking $vscanbin !\n";
   }
+
+  return;
 }
 
-sub rfc2822_date() {
+sub rfc2822_date {
   # credits to Chip Rosenthal <chip@unicom.com>
   # taken from pxytest
 
@@ -432,47 +455,59 @@ sub rfc2822_date() {
   "Jan", "Feb", "Mar", "Apr", "May", "Jun",
   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
 
-  sprintf("%-3s, %02d %-3s %4d %02d:%02d:%02d GMT",
+  return sprintf("%-3s, %02d %-3s %4d %02d:%02d:%02d GMT",
   $Day_name[$gm->wday],
   $gm->mday, $Month_name[$gm->mon], 1900+$gm->year,
   $gm->hour, $gm->min, $gm->sec);
 }
 
-sub content() {
+sub content {
+  my ($file) = @_;
   my $input;
-  if (-r $_[0]) {
-    open(FILE, $_[0]);
-      #$input = chomp(*FILE);    <- doesn't work !
-      $input = <FILE>;
+  if (-r $file) {
+    open(my $FILEFH, "<", $file) || croak "Could not read from $file";
+      $input = <$FILEFH>;
       if ($input) { chomp($input); }
-    close(FILE);
+    close($FILEFH);
   } else {
     $input = '';
   }
   return $input;
 }
 
-sub connect_mysql() {
+sub connect_mysql {
   $db = "dbi:mysql:qbox"; #; host='".&content("$configdir\/dbserver")."'";
   $dbh = DBI->connect($db, &content("$configdir\/dbuser"), &content("$configdir\/dbpass"));
+
+  if ($dbh) {
+    return 1;
+  }
+  return 0;
 }
 
-sub rewrite_domain() {
+sub rewrite_domain {
   $statement = "select rewrite from domains where domain = \"$domain\" and rewrite != ''";
   &make_query();
   if ($sqlrows > 0) {
     @data = $sth->fetchrow_array();
     $domain = $data[0];
   }
+
+  return;
 }
 
-sub debug() {
-  if ($ENV{'QBOX_DEBUG'}) { print STDERR $_[0] };
+sub debug {
+  my ($lmessage) = @_;
+  if ($ENV{'QBOX_DEBUG'}) { print STDERR $lmessage };
+
+  return;
 }
 
-sub duplicate() {
-  if ($File::Find::name =~ /\.$msghash/) {
+sub duplicate {
+  if ($File::Find::name =~ /\.$msghash/x) {
     &debug("$File::Find::name is a duplocate with hash value $msghash\n");
     $isduplicate = 1;
   }
+
+  return;
 }
