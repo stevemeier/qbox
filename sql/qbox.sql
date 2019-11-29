faaadba8589f5002822454aeb5f8959acf9760e3
stevemeier <email@steve-meier.de>
2019-11-28 22:01:40 -0500

    Added SQL schema to repo

---
+++ sql/qbox.sql
@@ -0,0 +1,226 @@
+-- MySQL dump 10.14  Distrib 5.5.64-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: qbox
+-- ------------------------------------------------------
+-- Server version	5.5.64-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `qbox`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `qbox` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `qbox`;
+
+--
+-- Table structure for table `autoconfig`
+--
+
+DROP TABLE IF EXISTS `autoconfig`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `autoconfig` (
+  `field` text,
+  `value` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `collector`
+--
+
+DROP TABLE IF EXISTS `collector`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `collector` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL DEFAULT '0',
+  `username` varchar(128) NOT NULL DEFAULT '',
+  `password` varchar(128) NOT NULL DEFAULT '',
+  `server` varchar(255) NOT NULL DEFAULT '',
+  `protocol` varchar(4) NOT NULL DEFAULT '',
+  `check_every` int(11) NOT NULL DEFAULT '0',
+  `lastcheck` int(11) NOT NULL DEFAULT '0',
+  `lastsuccess` int(11) NOT NULL DEFAULT '0',
+  `lastfail` int(11) NOT NULL DEFAULT '0',
+  `lasterror` tinyint(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `domains`
+--
+
+DROP TABLE IF EXISTS `domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `domains` (
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `rewrite` varchar(255) DEFAULT NULL,
+  `uid` bigint(20) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`domain`),
+  UNIQUE KEY `domain` (`domain`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `filter`
+--
+
+DROP TABLE IF EXISTS `filter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filter` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL DEFAULT '0',
+  `applyto` tinyint(4) NOT NULL DEFAULT '0',
+  `filter` bigint(20) NOT NULL DEFAULT '0',
+  `destination` varchar(255) NOT NULL DEFAULT '',
+  `value` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `filters`
+--
+
+DROP TABLE IF EXISTS `filters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `filters` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `expression` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lastlogin`
+--
+
+DROP TABLE IF EXISTS `lastlogin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lastlogin` (
+  `uid` bigint(20) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `epoch` bigint(20) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `protocol` text,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mapping`
+--
+
+DROP TABLE IF EXISTS `mapping`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mapping` (
+  `user` varchar(255) NOT NULL DEFAULT '',
+  `domain` varchar(255) NOT NULL DEFAULT '',
+  `uid` bigint(20) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `passwd`
+--
+
+DROP TABLE IF EXISTS `passwd`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `passwd` (
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '',
+  `uid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `gid` bigint(20) NOT NULL DEFAULT '0',
+  `realname` varchar(255) NOT NULL DEFAULT '',
+  `comment` varchar(255) NOT NULL DEFAULT '',
+  `homedir` varchar(255) NOT NULL DEFAULT '',
+  `spamdir` varchar(255) DEFAULT NULL,
+  `spamlimit` int(3) DEFAULT NULL,
+  `sysuid` int(11) NOT NULL DEFAULT '8',
+  `sysgid` int(11) NOT NULL DEFAULT '8',
+  `quota` bigint(20) NOT NULL DEFAULT '0',
+  `smtp` char(3) NOT NULL DEFAULT 'yes',
+  `pop` char(3) NOT NULL DEFAULT 'yes',
+  `pops` char(3) NOT NULL DEFAULT 'yes',
+  `imap` char(3) NOT NULL DEFAULT 'yes',
+  `imaps` char(3) NOT NULL DEFAULT 'yes',
+  `extmail` varchar(255) NOT NULL DEFAULT '',
+  `antivir` int(11) NOT NULL DEFAULT '0',
+  `antispam` int(11) NOT NULL DEFAULT '0',
+  `arstart` bigint(20) NOT NULL DEFAULT '0',
+  `arend` bigint(20) NOT NULL DEFAULT '0',
+  `artext` text,
+  `prquestion` varchar(255) NOT NULL DEFAULT '',
+  `pranswer` varchar(64) NOT NULL DEFAULT '',
+  `open` bigint(20) NOT NULL DEFAULT '0',
+  `close` bigint(20) NOT NULL DEFAULT '0',
+  `locked` tinyint(4) NOT NULL DEFAULT '0',
+  `dupfilter` tinyint(4) NOT NULL DEFAULT '0',
+  `oath_token` varchar(64) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `responses`
+--
+
+DROP TABLE IF EXISTS `responses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `responses` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `uid` bigint(20) NOT NULL DEFAULT '0',
+  `rcpt` varchar(255) NOT NULL DEFAULT '',
+  `time` bigint(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `sendlog`
+--
+
+DROP TABLE IF EXISTS `sendlog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sendlog` (
+  `msgid` bigint(20) NOT NULL,
+  `unixtime` bigint(20) NOT NULL,
+  `size` bigint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-29  3:51:27
