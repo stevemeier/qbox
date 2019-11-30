7a93e698d475c536ed4c1b7c79b2065bce07ebb7
stevemeier <email@steve-meier.de>
2019-11-29 19:40:47 -0500

    More experimenting with alias_of

--- checkpassword-server.go
+++ checkpassword-server.go
@@ -206,6 +206,9 @@ func authenticate(w http.ResponseWriter, r *http.Request) {
 				}
 			}
 			rows2.Close()
+
+//			reqdata.Username = dbdata.aliasof
+//			^^ Dovecot does not like the username being changed
 		}
 
 		// Send response to checkpassword-client
