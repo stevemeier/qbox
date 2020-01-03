669910cc7d81f7a2b44b7e437ddbc76c24ed1670
stevemeier <email@steve-meier.de>
2020-01-03 20:35:01 +0100

    Improve test system setup

--- tests/setup_testsystem.sh
+++ tests/setup_testsystem.sh
@@ -15,16 +15,28 @@ yum -y install spamassassin clamav
 freshclam
 service spamassassin start
 
+# Clone and build code
+cd /opt
+git clone https://github.com/stevemeier/qbox.git
+cd qbox
+make
+
+# Prepare config
 mkdir /etc/qbox
 echo -n root > /etc/qbox/dbuser
 echo -n > /etc/qbox/dbpass
 
+# Prepare filesystem
 mkdir -p /home/mail/testuser1/INBOX/new /home/mail/testuser1/INBOX/tmp /home/mail/testuser1/INBOX/cur
 mkdir -p /home/mail/testuser2/INBOX/new /home/mail/testuser2/INBOX/tmp /home/mail/testuser2/INBOX/cur
 mkdir -p /home/mail/testuser3/INBOX/new /home/mail/testuser3/INBOX/tmp /home/mail/testuser3/INBOX/cur
 mkdir -p /home/mail/testuser4/INBOX/new /home/mail/testuser4/INBOX/tmp /home/mail/testuser4/INBOX/cur
 chown -R mail:mail /home/mail
 
+# Load SQL schema
+mysql < /opt/qbox/sql/qbox.sql
+
+# Put test data into SQL
 echo 'INSERT INTO qbox.passwd (username, password, homedir, antispam, antivir) VALUES ("testuser1","testpass1","/home/mail/testuser1",0,0);' | mysql
 echo 'INSERT INTO qbox.passwd (username, password, homedir, antispam, antivir) VALUES ("testuser2","testpass2","/home/mail/testuser2",1,0);' | mysql
 echo 'INSERT INTO qbox.passwd (username, password, homedir, antispam, antivir) VALUES ("testuser3","testpass3","/home/mail/testuser3",0,1);' | mysql
@@ -48,9 +60,3 @@ echo 'INSERT INTO qbox.mapping VALUES ("testusers","localhost",2,"");' | mysql
 echo 'INSERT INTO qbox.mapping VALUES ("testusers","localhost",3,"");' | mysql
 echo 'INSERT INTO qbox.mapping VALUES ("testusers","localhost",4,"");' | mysql
 echo 'INSERT INTO qbox.mapping VALUES ("devnull","localhost",8,"");' | mysql
-
-# Clone and build code
-cd /opt
-git clone https://github.com/stevemeier/qbox.git
-cd qbox
-make
