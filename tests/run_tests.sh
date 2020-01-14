8af22301e86eabad17ac1e91a843c7d797c907fe
stevemeier <email@steve-meier.de>
2020-01-14 22:38:44 +0100

    Add Eicar example and Antivir basic test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,3 +3,9 @@
 # No Filter Tests
 for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=nofilter@localhost /opt/qbox/deliver < ${I}; done
 
+# Antispam Tests
+for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=antispam@localhost /opt/qbox/deliver < ${I}; done
+grep -h ^X-Spam-Checker-Version /home/mail/testuser2/INBOX/new/* | uniq -c
+
+# Antivir Tests
+for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=antivir@localhost /opt/qbox/deliver < ${I}; done
