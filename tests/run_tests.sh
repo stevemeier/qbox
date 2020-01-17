390c36f8fe447b49447d936624ccabec02319e67
stevemeier <email@steve-meier.de>
2020-01-17 21:24:00 +0100

    More tests added

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,6 +2,7 @@
 
 # No Filter Tests
 for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=nofilter@localhost /opt/qbox/deliver < ${I}; done
+find /home/mail/testuser1 -type f | wc -l
 
 # Antispam Tests
 for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=antispam@localhost /opt/qbox/deliver < ${I}; done
@@ -9,3 +10,8 @@ grep -h ^X-Spam-Checker-Version /home/mail/testuser2/INBOX/new/* | uniq -c
 
 # Antivir Tests
 for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=antivir@localhost /opt/qbox/deliver < ${I}; done
+find /home/mail/testuser3 -type f | wc -l
+
+# Antispam + Antivir Tests
+for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=spamvir@localhost /opt/qbox/deliver < ${I}; done
+find /home/mail/testuser4 -type f | wc -l
