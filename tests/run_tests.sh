84a19a47e72cc7f888c87ed81560fffbd3abc42b
stevemeier <email@steve-meier.de>
2020-01-18 21:38:08 +0100

    More tests...

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -39,3 +39,16 @@ echo '*** START: PIPE TRUE ***'
 for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=true@localhost /opt/qbox/deliver < ${I}; echo $?; done
 echo '*** END: PIPE TRUE ***'
 echo
+
+# Pipe to `true` and `false`
+echo '*** START: PIPE TRUE+FALSE ***'
+for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=truefalse@localhost /opt/qbox/deliver < ${I}; echo $?; done
+echo '*** END: PIPE TRUE+FALSE ***'
+echo
+
+# Test dupfilter
+echo '*** START: DUPFILTER ***'
+for I in `find /opt/qbox/tests/emails -name '*.eml' -type f`; do RECIPIENT=dupfilter@localhost /opt/qbox/deliver < ${I}; echo $?; done
+find /home/mail/testuser9 -type f | wc -l
+echo '*** END: DUPFILTER ***'
+echo
