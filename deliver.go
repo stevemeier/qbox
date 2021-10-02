c7a02affca292590e858b44019e1cef9991a7d7a
stevemeier <email@steve-meier.de>
2021-10-02 23:26:27 +0200

    Small typo fix

--- deliver.go
+++ deliver.go
@@ -193,7 +193,7 @@ func main() {
 				// Virus was found, strip attachments
 				message.Object.Attachments = nil
 				// Add a security note instead
-				message.Object.Attach(strings.NewReader("Attachments have been removed by virus scanner"), "security_notice.txt", "text/plain ")
+				message.Object.Attach(strings.NewReader("Attachments have been removed by virus scanner"), "security_notice.txt", "text/plain")
 			}
 		}
 	}
